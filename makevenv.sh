3a48f3a230b24a49c46e98ad1743e11c0ae0c78d
nimirium <sophie.fogel@gmail.com>
2019-06-22 13:03:13 +0300

    done!

--- makevenv.sh
+++ makevenv.sh
@@ -9,30 +9,43 @@ else
     read -p "venv name: " venv_name
 fi
 
-#echo "venv name from input: $venv_name"
-
 virtualenv -p ${PYTHON_V} ${venv_name}
 
-read -n 1 -p "
-Install requirements file [y/n]? " install_req_file
+read -n 1 -p ">> Install requirements file [y/n]? " install_req_file
+echo ""
 
-if [[ ${install_req_file} == "y" ]]
-then
-    read -p "
-    requirements file path: " file_path
+while [[ ${install_req_file} == "y" ]]
+do
+    read -p ">> Requirements file path: " file_path
     echo "Installing requirements from file: ${file_path}"
     ${venv_name}/bin/pip install -r ${file_path}
-fi
 
-read -n 1 -p "
-Install libraries [y/n]? " install_lib
+    read -n 1 -p ">> Install another requirements file [y/n]? " install_req_file
+    echo ""
+done
 
-if [[ ${install_lib} == "y" ]]
-then
-    read -p "
-    Please input library, with or without version: " lib_name
-    echo "Installing library: ${lib_name}"
+read -n 1 -p ">> Install libraries [y/n]? " install_lib
+echo ""
+
+while [[ ${install_lib} == "y" ]]
+do
+    read -p ">> Please input library, with or without version: " lib_name
+    echo "Installing library: ${lib_name}
+"
     ${venv_name}/bin/pip install ${lib_name}
+
+    read -n 1 -p ">> Install more libraries [y/n]? " install_lib
+    echo ""
+done
+
+read -n 1 -p ">> Print virtual environment libraries (pip freeze) [y/n]? " print_pip_freeze
+echo ""
+
+if [[ ${print_pip_freeze} == "y" ]]
+then
+    echo ""
+    ${venv_name}/bin/pip freeze
+    echo ""
 fi
 
 echo "Done! Created virtual environment $venv_name, using python version $PYTHON_V"
